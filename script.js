b6f57cc04dad5c7f43dbafeb4336ddb5ba244b24
Leonardo Hernandez <leonardo.hernandezm@yahoo.com>
2023-03-06 21:54:18 -0500

    Commit 3 Mar 3

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 let buttonEdit = document.querySelector(".editbutton__pencil");
 let popup = document.querySelector(".popup");
-let infoName = document.querySelector(".profile__info-name");
-let infoJob = document.querySelector(".profile__info-description");
-let profileName = document.querySelector(".popup__input1");
-let profileJob = document.querySelector(".popup__input2");
+/*let infoName = document.querySelector(".profile__info-name");*/
+/*let infoJob = document.querySelector(".profile__info-description");*/
+let profileName = document.querySelector(".profile__info-name");
+let profileJob = document.querySelector(".profile__info-description");
 let buttonCloseIcon = document.querySelector(".closeicon");
-let inputName = document.querySelector(".profile__info-name");/*#fieldname*/
-let inputJob = document.querySelector(".profile__info-description");/*#fieldjob*/
+let inputName = document.querySelector(".popup__input-name");/*#fieldname*/
+let inputJob = document.querySelector(".popup__input-job");/*#fieldjob*/
 let buttonSubmit = document.querySelector(".popup__submit-button");
 /*let profileName = document.querySelector(".popup__input1");
 let profileJob = document.querySelector(".popup__input2");*/
@@ -16,8 +16,8 @@ let profileJob = document.querySelector(".popup__input2");*/
 
 buttonEdit.addEventListener("click", function () {
     popup.classList.add("open__popup");
-    profileName.textContent = infoName.value;
-    profileJob.textContent = infoJob.value;      
+    inputName.value = profileName.textContent;
+    inputJob.value = profileJob.textContent;   
 })
 
 buttonCloseIcon.addEventListener("click", function () {
@@ -26,8 +26,8 @@ buttonCloseIcon.addEventListener("click", function () {
 
 buttonSubmit.addEventListener("click", function (event){
     event.preventDefault();
-    inputName.textContent = profileName.value;
-    inputJob.textContent = profileJob.value;
+    profileName.textContent = inputName.value;
+    profileJob.textContent = inputJob.value;
     popup.classList.remove("open__popup");
 })
 
